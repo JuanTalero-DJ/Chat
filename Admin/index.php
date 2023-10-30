a6f91fb83e57b9bd3d207c486cf242431ebd7a3f
Paula Sanchez <paulacamilapc0@gmail.com>
2023-10-29 19:28:49 -0500

    desbloquear

--- Admin/index.php
+++ Admin/index.php
@@ -6,24 +6,26 @@
         body {
             font-family: Arial, sans-serif;
             background-color: #f0f0f0;
+            text-align: center;
         }
         h1 {
-            text-align: center;
+            margin-top: 20px;
         }
         h2 {
             margin-top: 20px;
         }
         table {
-            width: 100%;
+            width: 80%;
             border-collapse: collapse;
-            margin-top: 20px;
+            margin: 20px auto;
+            box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
         }
         th, td {
-            padding: 8px;
+            padding: 12px 15px;
             text-align: left;
         }
         th {
-            background-color: #333;
+            background-color: #0074b1;
             color: #fff;
         }
         tr:nth-child(even) {
@@ -54,6 +56,10 @@
         input[type="submit"]:hover {
             background-color: #00548c;
         }
+        .blocked {
+            color: red;
+            font-weight: bold;
+        }
     </style>
 </head>
 <body>
@@ -65,56 +71,91 @@
             <tr>
                 <th>ID Usuario</th>
                 <th>Nombre de Usuario</th>
+                <th>Estado</th>
                 <th>Nueva Contraseña</th>
                 <th>Acción</th>
             </tr>
             <?php
-             
-             include_once '../BaseDatos/DbConection.php';
-             $conn = conexion();
- 
-             if (!$conn) {
-                 die("Error al conectar a la base de datos: " . mysqli_connect_error());
-             }
- 
-             $consulta = "SELECT IdUsuario, Log FROM usuario";
-             $resultado = mysqli_query($conn, $consulta);
- 
-             if ($resultado) {
-                 while ($fila = mysqli_fetch_assoc($resultado)) {
-                     echo "<tr>";
-                     echo "<td>{$fila['IdUsuario']}</td>";
-                     echo "<td>{$fila['Log']}</td>";
-                     echo "<td><input type='password' name='nueva_contrasena[{$fila['IdUsuario']}]' value=''></td>";
-                     echo "<td><button type='submit' name='modificar' value='{$fila['IdUsuario']}'>Modificar</button></td>";
-                     echo "</tr>";
-                 }
-             } else {
-                 echo "Error al ejecutar la consulta: " . mysqli_error($conn);             }
- 
-             if (isset($_POST['modificar'])) {
-                 foreach ($_POST['nueva_contrasena'] as $id_usuario => $nueva_contrasena) {
-                     $id_usuario = intval($id_usuario);
-                     $nueva_contrasena = mysqli_real_escape_string($conn, $nueva_contrasena);
-                     
-                     if (!empty($nueva_contrasena)) {
-                         $actualizar_query = "UPDATE usuario SET Contraseña = '$nueva_contrasena' WHERE IdUsuario = $id_usuario";
- 
-                         if (mysqli_query($conn, $actualizar_query)) {
-                             echo "<p>Contraseña actualizada con éxito para el usuario con ID $id_usuario.</p>";
-                             
-                         } else {
-                             echo "<p>Error al actualizar la contraseña para el usuario con ID $id_usuario: " . mysqli_error($conn) . "</p>";
-                         }
-                     }
-                 }
-             }
- 
-             mysqli_close($conn);
-             ?>
-           
+            include_once '../BaseDatos/DbConection.php';
+            $conn = conexion();
+
+            if (!$conn) {
+                die("Error al conectar a la base de datos: " . mysqli_connect_error());
+            }
+
+            $consulta = "SELECT IdUsuario, Log, bloqueado FROM usuario";
+            $resultado = mysqli_query($conn, $consulta);
+
+            if ($resultado) {
+                while ($fila = mysqli_fetch_assoc($resultado)) {
+                    $idUsuario = $fila['IdUsuario'];
+                    $username = $fila['Log'];
+                    $isBlocked = $fila['bloqueado'] == 1 ? true : false;
+
+                    echo "<tr>";
+                    echo "<td>{$idUsuario}</td>";
+                    echo "<td>{$username}</td>";
+                    echo "<td class='" . ($isBlocked ? "blocked" : "") . "'>" . ($isBlocked ? "Bloqueado" : "Desbloqueado") . "</td>";
+                    echo "<td><input type='password' name='nueva_contrasena[{$idUsuario}]' value=''></td>";
+                    echo "<td>";
+                    if ($isBlocked) {
+                        echo "<button type='submit' name='desbloquear' value='{$idUsuario}'>Desbloquear</button>";
+                    } else {
+                        echo "<button type='submit' name='modificar' value='{$idUsuario}'>Modificar</button>";
+                    }
+                    echo "</td>";
+                    echo "</tr>";
+                }
+            } else {
+                echo "Error al ejecutar la consulta: " . mysqli_error($conn);
+            }
+
+            if (isset($_POST['modificar'])) {
+                foreach ($_POST['nueva_contrasena'] as $idUsuario => $nueva_contrasena) {
+                    $idUsuario = intval($idUsuario);
+                    $nueva_contrasena = mysqli_real_escape_string($conn, $nueva_contrasena);
+
+                    if (!empty($nueva_contrasena)) {
+                        $actualizar_query = "UPDATE usuario SET Contraseña = '$nueva_contrasena' WHERE IdUsuario = $idUsuario";
+
+                        if (mysqli_query($conn, $actualizar_query)) {
+                            echo "<p>Contraseña actualizada con éxito para el usuario con ID $idUsuario.</p>";
+                        } else {
+                            echo "<p>Error al actualizar la contraseña para el usuario con ID $idUsuario: " . mysqli_error($conn) . "</p>";
+                        }
+                    }
+                }
+            }
+
+            if (isset($_POST['desbloquear'])) {
+                $idUsuario = $_POST['desbloquear'];
+                $desbloquear_query = "UPDATE usuario SET bloqueado = 0 WHERE IdUsuario = $idUsuario";
+                
+                if (mysqli_query($conn, $desbloquear_query)) {
+                    echo "<p>Usuario desbloqueado con éxito.</p>";
+                } else {
+                    echo "<p>Error al desbloquear al usuario: " . mysqli_error($conn) . "</p>";
+                }
+                
+            }
+
+            if (isset($_POST['desbloquear'])) {
+                $idUsuario = $_POST['desbloquear'];
+                $desbloquear_query = "UPDATE usuario SET bloqueado = 0, intentos_fallidos = 0 WHERE IdUsuario = $idUsuario";
+                
+                if (mysqli_query($conn, $desbloquear_query)) {
+                    
+                } else {
+                    echo "<p>Error al desbloquear al usuario: " . mysqli_error($conn) . "</p>";
+                }
+            }
+            
+
+            mysqli_close($conn);
+            ?>
         </table>
         <input type="submit" value="Guardar Cambios">
+        <input type="button" value="Salir" onclick="window.location.href='../index.html'">
     </form>
 </body>
 </html>
--- Logo/LogIn.php
+++ Logo/LogIn.php
@@ -1,30 +1,50 @@
 <?php
 include_once '../BaseDatos/DbConection.php';
+
+session_start();
+
 $user = $_POST['user'];
 $pass = $_POST['pass'];
 $conn = conexion();
 
-$sql = "SELECT * FROM usuario WHERE Log = '$user' AND Contraseña = '$pass'";
-$resultado_usuario = mysqli_query($conn, $sql);
 
-$sql_admin = "SELECT * FROM usuarioadmin WHERE Logadmin = '$user' AND contraseñadmin = '$pass'";
+$sql_usuario = "SELECT * FROM usuario WHERE Log = '$user'";
+$resultado_usuario = mysqli_query($conn, $sql_usuario);
+
+
+$sql_admin = "SELECT * FROM usuarioadmin WHERE Logadmin = '$user'";
 $resultado_admin = mysqli_query($conn, $sql_admin);
 
-$data_usuario = mysqli_fetch_assoc($resultado_usuario);
-$data_admin = mysqli_fetch_assoc($resultado_admin);
+if ($resultado_usuario && mysqli_num_rows($resultado_usuario) > 0) {
+    $data_usuario = mysqli_fetch_assoc($resultado_usuario);
 
-if ($data_usuario == null && $data_admin == null) {
-  echo '<script type="text/javascript">alert("El usuario o contraseña ingresados no son válidos");window.location.href="../index.html";</script>';
+    if ($data_usuario['bloqueado'] == 1) {
+        echo '<script type="text/javascript">alert("Tu cuenta está bloqueada. Contacta al administrador.");window.location.href="../index.html";</script>';
+    } else {
+        if ($data_usuario['Contraseña'] == $pass) {
+            mysqli_query($conn, "UPDATE usuario SET intentos_fallidos = 0 WHERE Log = '$user'");
+            $_SESSION["user"] = $data_usuario;
+            header('Location:../Chats/index.php');
+        } else {
+            $intentos_fallidos = $data_usuario['intentos_fallidos'] + 1;
+            mysqli_query($conn, "UPDATE usuario SET intentos_fallidos = $intentos_fallidos WHERE Log = '$user'");
+            if ($intentos_fallidos >= 3) {
+                mysqli_query($conn, "UPDATE usuario SET bloqueado = 1, bloqueo_fecha = NOW() WHERE Log = '$user'");
+            }
+            echo '<script type="text/javascript">alert("El usuario o contraseña ingresados no son válidos.");window.location.href="../index.html";</script>';
+        }
+    }
+} elseif ($resultado_admin && mysqli_num_rows($resultado_admin) > 0) {
+    $data_admin = mysqli_fetch_assoc($resultado_admin);
+
+    if ($data_admin['contraseñadmin'] == $pass) {
+        $_SESSION["admin"] = $data_admin;
+        header('Location:../Admin/index.php');
+    } else {
+        echo '<script type="text/javascript">alert("La contraseña del administrador es incorrecta.");window.location.href="../index.html";</script>';
+    }
 } else {
-  session_start();
-  
-  if ($data_usuario !== null) {
-    $_SESSION["user"] = $data_usuario;
-    header('Location:../Chats/index.php');
-  } elseif ($data_admin !== null) {
-    $_SESSION["user"] = $data_admin;
-    header('Location:../Admin/index.php'); 
-  }
+    echo '<script type="text/javascript">alert("El usuario no existe.");window.location.href="../index.html";</script>';
 }
 
 mysqli_free_result($resultado_usuario);
